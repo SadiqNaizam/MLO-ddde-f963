2304afb8f9c0adaecbda06e88dffe696b23a43ac
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-18 16:10:15 +0530

    Files generated: tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -68,6 +68,9 @@ export default {
 				md: 'calc(var(--radius) - 2px)',
 				sm: 'calc(var(--radius) - 4px)'
 			},
+			fontFamily: {
+        sans: ['var(--font-sans)', 'system-ui', -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+      },
 			keyframes: {
 				'accordion-down': {
 					from: {
